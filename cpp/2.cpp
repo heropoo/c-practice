338e54b71df8496f5ba9475e0c62ff239318b260
heropoo <aiyouyou1000@163.com>
2020-07-11 00:49:52 +0800

    test

--- cpp/2.cpp
+++ cpp/2.cpp
@@ -2,57 +2,6 @@
 
 using namespace std;
 
-<<<<<<< HEAD
-class Box
-{
-public:
-    double length;
-    double width;
-    double height;
-    Box();  //构造函数 与类同名
-    void say(char *msg);    
-};
-
-// 成员函数定义，包括构造函数
-Box::Box()
-{
-    cout << "Object is being created" << endl;
-}
-
-void Box::say(char *msg){
-    cout << msg <<endl;
-}
-
-
-
-int main()
-{
-    Box Box1;            // 声明 Box1，类型为 Box
-    Box Box2;            // 声明 Box2，类型为 Box
-    double volume = 0.0; // 用于存储体积
-
-
-
-     // box 1 详述
-   Box1.height = 5.0; 
-   Box1.length = 6.0; 
-   Box1.width = 7.0;
- 
-   // box 2 详述
-   Box2.height = 10.0;
-   Box2.length = 12.0;
-   Box2.width = 13.0;
-
-   // box 1 的体积
-   volume = Box1.height * Box1.length * Box1.width;
-   cout << "Box1 的体积：" << volume <<endl;
-
-    // box 2 的体积
-   volume = Box2.height * Box2.length * Box2.width;
-   cout << "Box2 的体积：" << volume <<endl;
-
-   Box1.say("fsdfsdf");
-=======
 // 基类
 class Shape
 {
@@ -105,7 +54,6 @@ int main(void)
     Tri.setHeight(7);
     // 输出对象的面积
     cout << "Total Triangle area: " << Tri.getArea() << endl;
->>>>>>> 1c63da0264ac08aadc4f95bf76b5340b6ddd5a09
 
     return 0;
 }
--- cpp/3.cpp
+++ cpp/3.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
  
 using namespace std;
 
@@ -41,37 +40,5 @@ int main()
     line.setLength(6.0);
     cout << "Length of line : " << line.getLength() << endl;
 
-=======
-using namespace std;
-
-class Adder
-{
-public:
-    Adder(int i = 0) : total(i) {}
-
-    void addNum(int i)
-    {
-        total += i;
-    }
-
-    int getTotal()
-    {
-        return total;
-    }
-
-private:
-    int total;
-};
-
-int main()
-{
-    Adder a;
-
-    a.addNum(10);
-    a.addNum(20);
-    a.addNum(30);
-
-    cout << "Total " << a.getTotal() << endl;
->>>>>>> 1c63da0264ac08aadc4f95bf76b5340b6ddd5a09
     return 0;
 }
